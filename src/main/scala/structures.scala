9b58e67764493386dfda4dfa361e5182ed3e9fcb
Griffin Moe <me@griffinmoe.com>
2015-01-31 18:51:53 -0600

    Rebuilt Pitch to distinguish enharmonic notes.

--- src/main/scala/structures.scala
+++ src/main/scala/structures.scala
@@ -2,52 +2,52 @@ package music_dsl
 
 import scalaz._, Scalaz._
 
-//TODO: Implement tuplets, maybe staves/voices, dynamics
+//TODO: Implement tuplets, staves/voices, dynamics
 object structures {
 
   sealed trait Music
   case class Beat(value: (Int,Int) = (1,4)) extends Music {
     require((value._2 & (value._2 - 1)) == 0)
   }
-  case class Pitch(value: Int = 0) extends Music {
-    require(value >= -48 && value <= 48) // 0 = Middle C
-  }
+  case class Pitch(pitchClass: PitchClass.PitchClass, 
+    decorator: PitchDecorator.PitchDecorator, octave: Int) extends Music 
   case class Note(pitch: Pitch, duration: Beat) extends Music
-  case class PitchSet(pitches: Pitch*) extends Music with Traversable[Pitch] {
-    def foreach[U](f: Pitch => U) = pitches foreach f
-  }
+  case class PitchSet(pitches: Pitch*) extends Music 
   case class Chord(pitches: PitchSet, duration: Beat) extends Music
-  case class Measure(timeSig: (Int,Int), music: Music*) extends Music with Traversable[Music] {
-    def foreach[U](f: Music => U) = music foreach f
+  case class Measure(timeSig: (Int,Int), music: Music*) extends Music 
+
+  object PitchClass extends Enumeration {
+    type PitchClass = Value
+    val C, D, E, F, G, A, B = Value
+
+    val pClass = Map("c" -> C, "d" -> D, "e" -> E, "f" -> F,
+      "g" -> G, "a" -> A, "b" -> B)
+    
+    def fromString(s: String):PitchClass = pClass.getOrElse(s.toLowerCase,C)
+  }
+
+  object PitchDecorator extends Enumeration {
+    type PitchDecorator = Value
+    val Blank, Natural, Sharp, Flat, DoubleSharp, DoubleFlat = Value
+
+    val dec = Map("n" -> Natural, "#" -> Sharp, "-" -> Flat, "x" -> DoubleSharp, 
+      "##" -> DoubleSharp, "_" -> DoubleFlat)
+
+    def fromString(s: String):PitchDecorator = dec.getOrElse(s.toLowerCase,Blank)
   }
 
   object Pitch {
-    val pClass = Map('c' -> 0, 'd' -> 2, 'e' -> 4, 'f' -> 5,
-      'g' -> 7, 'a' -> 9, 'b' -> 11)
-    val dec = Map('#' -> 1, '-' -> -1, 'x' -> 2, ''' -> 12, ',' -> -12)
+    import PitchClass._
+    import PitchDecorator._
 
-    def apply(s:String):Pitch = new Pitch(pClass.getOrElse(s(0).toLower,0)+
-        (s.drop(0) collect {case c:Char => dec.getOrElse(c.toLower,0)}).sum
-    )
+    val r = """([a-g,A-G])([n|#|##|x|X|-|_]?)([,|']*)""".r
+
+    def apply(s:String): Pitch = s match {
+      case r(p) =>     new Pitch(PitchClass.fromString(p),Blank,0)
+      case r(p,d) =>   new Pitch(PitchClass.fromString(p),PitchDecorator.fromString(d), 0)
+      case r(p,d,o) => new Pitch(PitchClass.fromString(p),PitchDecorator.fromString(d),o.size)
+      case _ => new Pitch(C, Blank, 0)
+    }
   }
 
-  object PitchSet {
-    def apply(s: String): PitchSet = new PitchSet(
-      s.split(" ") collect {case p:String => Pitch(p)}:_*
-    )
-  }
-
-  def playNote(n: Note): Note = {
-    import javax.sound.midi._
-
-    val synth = MidiSystem.getSynthesizer()
-    synth.open()
-
-    val channel = synth.getChannels().head
-    channel.noteOn(60 + n.pitch.value, 110)
-    Thread.sleep((2000.0 * (n.duration.value._1.doubleValue / 
-      n.duration.value._2.doubleValue)).longValue)
-    channel.noteOff(60 + n.pitch.value, 110)
-    n
-  }
 }
--- src/test/scala/StructTests.scala
+++ src/test/scala/StructTests.scala
@@ -6,11 +6,18 @@ class StructTests extends FunSuite {
 
   import structures._
 
-  test("Basic Pitch - String interpetation") {
-    val p1 = Pitch(14)
-    val p2 = Pitch("c##'")
-    val p3 = Pitch("cx'")
+  test("Pitch - String interpetation") {
+    val p1 = Pitch(PitchClass.D,PitchDecorator.Blank,0)
+    val p2 = Pitch("d")
 
-    assert((p1 == p2) && (p2 == p3))
+    assert(p1 == p2)
+  }
+  
+  test("Pitch - String intepretation w/ decorator") {
+    assert(Pitch(PitchClass.D,PitchDecorator.Sharp,0) == Pitch("d#"))
+  }
+
+  test("Pitch - String intepretation w/ decorator + octave") {
+    assert(Pitch(PitchClass.D,PitchDecorator.Sharp,2) == Pitch("d#''"))
   }
 }
